597cee95114bd37e77163d42d41b57ef16d3c30a
Bui Thai Ha <habt42@live.com>
2016-11-15 10:12:41 +0000

    sign up

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,10 +1,26 @@
 class User < ApplicationRecord
-  has_many :active_relationships, ClassName: Relationship.name,
-    foreign_key: "follower_id", dependent: destroy
-  has_many :pasive_relationships, ClassName: Relationship.name,
-    foreign_key: "followed_id", dependent: destroy
+  has_many :active_relationships, class_name: Relationship.name,
+    foreign_key: "follower_id", dependent: :destroy
+  has_many :pasive_relationships, class_name: Relationship.name,
+    foreign_key: "followed_id", dependent: :destroy
   has_many :following, through: :active_relationships, source: :followed
   has_many :followers, through: :pasive_relationships, source: :follower
-  has_many :activities, dependent: destroy
-  has_many :lessons, dependent: destroy
+  has_many :activities, dependent: :destroy
+  has_many :lessons, dependent: :destroy
+
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
+
+  before_save :downcase_email
+
+  validates :name, presence: true, length: {maximum: 50}
+  validates :email, presence: true, length: {maximum: 255},
+    format: {with: VALID_EMAIL_REGEX}, uniqueness: {case_sensitive: false}
+  validates :password, length: {minimum: 6}, allow_nil: true
+
+  has_secure_password
+
+  private
+  def downcase_email
+    self.email = email.downcase
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
   root "static_pages#show", page_name: "home"
   get "/pages/:page_name" => "static_pages#show", as: :page
+  resources :users, only: [:new, :create]
 end
