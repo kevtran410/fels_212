2b979f4f521ea3cef192bd8f26c4aae510d71884
Tran Ngoc Nam <tran.ngoc.nam@framgia.com>
2016-11-16 08:18:19 +0700

    login-logout

--- db/schema.rb
+++ db/schema.rb
@@ -13,56 +13,56 @@
 ActiveRecord::Schema.define(version: 20161114150818) do
 
   create_table "activities", force: :cascade do |t|
-    t.integer "action_type"
-    t.integer "target_id"
-    t.integer "user_id"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.integer  "action_type"
+    t.integer  "target_id"
+    t.integer  "user_id"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
     t.index ["user_id", "created_at"], name: "index_activities_on_user_id_and_created_at"
     t.index ["user_id"], name: "index_activities_on_user_id"
   end
 
   create_table "answers", force: :cascade do |t|
-    t.string "content"
-    t.boolean "is_correct"
-    t.integer "word_id"
+    t.string   "content"
+    t.boolean  "is_correct"
+    t.integer  "word_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["word_id"], name: "index_answers_on_word_id"
   end
 
   create_table "categories", force: :cascade do |t|
-    t.string "name"
-    t.integer "duration"
+    t.string   "name"
+    t.integer  "duration"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
 
   create_table "lessons", force: :cascade do |t|
-    t.integer "spent_time"
+    t.integer  "spent_time"
     t.datetime "end_time"
-    t.integer "category_id"
-    t.integer "user_id"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.integer  "category_id"
+    t.integer  "user_id"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
     t.index ["category_id"], name: "index_lessons_on_category_id"
     t.index ["user_id"], name: "index_lessons_on_user_id"
   end
 
   create_table "relationships", force: :cascade do |t|
-    t.integer "follower_id"
-    t.integer "followed_id"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.integer  "follower_id"
+    t.integer  "followed_id"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
     t.index ["followed_id"], name: "index_relationships_on_followed_id"
     t.index ["follower_id", "followed_id"], name: "index_relationships_on_follower_id_and_followed_id", unique: true
     t.index ["follower_id"], name: "index_relationships_on_follower_id"
   end
 
   create_table "results", force: :cascade do |t|
-    t.integer "lesson_id"
-    t.integer "word_id"
-    t.integer "answer_id"
+    t.integer  "lesson_id"
+    t.integer  "word_id"
+    t.integer  "answer_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["answer_id"], name: "index_results_on_answer_id"
@@ -71,21 +71,21 @@ ActiveRecord::Schema.define(version: 20161114150818) do
   end
 
   create_table "users", force: :cascade do |t|
-    t.string "name"
-    t.string "email"
-    t.string "password_digest"
-    t.string "remember_digest"
-    t.boolean "is_admin", default: false
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.string   "name"
+    t.string   "email"
+    t.string   "password_digest"
+    t.string   "remember_digest"
+    t.boolean  "is_admin",        default: false
+    t.datetime "created_at",                      null: false
+    t.datetime "updated_at",                      null: false
     t.index ["email"], name: "index_users_on_email", unique: true
   end
 
   create_table "words", force: :cascade do |t|
-    t.string "content"
-    t.integer "category_id"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.string   "content"
+    t.integer  "category_id"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
     t.index ["category_id"], name: "index_words_on_category_id"
   end
 
